208690dbeeafb29209e5712604d8b62758b4befa
pamatron <117810440+pamatron@users.noreply.github.com>
2022-11-22 12:16:45 +0100

    fix getting version in build action

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
 
 int main(int argc, char **argv)
 {
-    std::cout << "This is my custom word count tool" << std::endl;
     MYWC::CLI obj(argc, argv);
 #ifndef NDEBUG
     std::cout << obj << std::endl;
