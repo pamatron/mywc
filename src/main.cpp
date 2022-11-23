71d51beb48e6ba8f042847c1a16dff58e06617de
pamatron <117810440+pamatron@users.noreply.github.com>
2022-11-23 15:35:08 +0100

    read from stdin and file

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,45 @@
 #include "commandline.h"
 
 #include <fmt/core.h>
+#include <fmt/ostream.h>
 
 #include <getopt.h>
+#include <iostream>
+#include <fstream>
+
+namespace MYWC
+{
+    void process_input(std::istream &in)
+    {
+        std::string line;
+        while (std::getline(in, line))
+        {
+            fmt::print("line: {}\n", line);
+        }
+    }
+}
 
 int main(int argc, char **argv)
 {
-    MYWC::CLI obj(argc, argv);
+    MYWC::CLI cli_args(argc, argv);
 #ifndef NDEBUG
-    fmt::print("{}\n",std::string(obj));
+    fmt::print("CLI args:\n{}\n", std::string(cli_args));
 #endif
+    if (std::string("-") == cli_args.file_name)
+    {
+        MYWC::process_input(std::cin);
+    }
+    else
+    {
+        std::ifstream input_file(cli_args.file_name);
+        if (input_file)
+        {
+            MYWC::process_input(input_file);
+        }
+        else
+        {
+            fmt::print(std::cerr, "could not open file \"{}\"\n",
+                       cli_args.file_name);
+        }
+    }
 }
